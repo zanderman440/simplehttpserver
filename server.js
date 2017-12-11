5bedf26f806182c990166dadf3d26c76894a50e8
zanderman440 <34091271+zanderman440@users.noreply.github.com>
2017-12-11 20:55:56 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var http = require("http");
-var port = process.env.port || 1337;
-
 var server = http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write("<!DOCTYPE "html">");
+  response.write("<html>");
+  response.write("<head>");
+  response.write("<title>Hello World Page</title>");
+  response.write("</head>");
+  response.write("<body>");
+  response.write("Hello World!");
+  response.write("</body>");
+  response.write("</html>");
+  response.end();
 });
 
-server.listen(port, 'localhost');
+server.listen(1337);
+console.log("Server is listening");
