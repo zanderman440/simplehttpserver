c4ed43913664042734e807b703a0d092bc387a69
zanderman440 <34091271+zanderman440@users.noreply.github.com>
2017-12-11 20:39:18 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,10 @@
-var sys = require("util"),
-my_http = require("http"),
-path = require("path"),
-url = require("url"),
-filesys = require("fs");
-my_http.createServer(function(request,response){
-    var my_path = url.parse(request.url).pathname;
-    var full_path = path.join(process.cwd(),my_path);
-    path.exists(full_path,function(exists){
-        if(!exists){
-            response.writeHeader(404, {"Content-Type": "text/plain"});
-            response.write("404 Not Found\n");
-            response.end();
-        }
-        else{
-            filesys.readFile(full_path, "binary", function(err, file) {
-                if(err) {
-                    response.writeHeader(500, {"Content-Type": "text/plain"});
-                    response.write(err + "\n");
-                    response.end();  
-                }
-                else{
-                    response.writeHeader(200);
-                    response.write(file, "binary");
-                    response.end();
-                }
-            });
-        }
-    });
-}).listen(6000);
-console.log("Server Running on 6000");
+var http = require("http");
+var port = process.env.port || 1337;
+
+var server = http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+});
+
+server.listen(port, 'localhost');
