0900b5e5fbacd695d16d5a5158c25fa7f3476b85
zanderman440 <34091271+zanderman440@users.noreply.github.com>
2017-12-11 20:23:28 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var http=require('http');
 http.createServer(function(req,res){
   res.writeHead(200,{'Content-Type':'text/plain'});
   res.end('Hello Node.js');
-}).listen(212,'127.0.0.1');
+}).listen(212,'localhost');
  
-console.log('Application running on http://127.0.0.1:212');
+console.log('Application running on http://localhost:212');
